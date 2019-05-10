713cfd4202b60d010e1b974faa9e97236aed4d01
andrea <andreadibenedetto92@gmail.com>
2019-05-10 12:31:01 +0200

    fix width height in jspdf

--- script/genPDF.js
+++ script/genPDF.js
@@ -4,10 +4,13 @@ function genPDF() {
 
   html2canvas(document.body).then(function (canvas) {
     var img = canvas.toDataURL('image/png');
-    var doc = new jsPDF();
+    var doc = new jsPDF("p", "mm", "a4");
 
     var width = doc.internal.pageSize.getWidth();
     var height = doc.internal.pageSize.getHeight();
+
+    console.log("width: "+width);
+    console.log("height: "+height);
 
     doc.addImage(img, 'JPEG', -2, 0, width, height);
     doc.save('Andrea Di Benedetto CV.pdf');
