abd54353618027b800f335f81aae527f22004c20
andrea <andreadibenedetto92@gmail.com>
2019-05-10 12:24:47 +0200

    fix width height in jspdf

--- script/genPDF.js
+++ script/genPDF.js
@@ -5,7 +5,11 @@ function genPDF() {
   html2canvas(document.body).then(function (canvas) {
     var img = canvas.toDataURL('image/png');
     var doc = new jsPDF();
-    doc.addImage(img, 'JPEG', -2, 0, 215, 275);
+
+    var width = doc.internal.pageSize.getWidth();
+    var height = doc.internal.pageSize.getHeight();
+
+    doc.addImage(img, 'JPEG', -2, 0, width, height);
     doc.save('Andrea Di Benedetto CV.pdf');
   });
 }
